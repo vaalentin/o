9645d4d264883196aac42ebe9c689e0aee19e8c3
Valentin Marmonier <valentin.marmonier@gmail.com>
2015-02-11 12:57:13 +0100

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -14,12 +14,19 @@ var __BASE__ = __dirname;
 var __SRC__ = __BASE__ + '/app/src';
 var __DIST__ = __BASE__ + '/app/dist';
 
-function vendor () {
-  var fs = require('fs');
-  var concat = require('gulp-concat');
-  var uglify = require('gulp-uglify');
-  var Promise = require('promise');
+var fs = require('fs');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var Promise = require('promise');
 
+/**
+ * Process vendors
+ * get the paths from package.json (browser entry)
+ * to dist/public/js
+ *
+ * @function vendor
+ */
+function vendor () {
   function getPkg () {
     return new Promise(function (resolve, reject) {
       fs.readFile('./package.json', { encoding: 'utf-8' }, function (err, data) {
@@ -37,20 +44,26 @@ function vendor () {
     return gulp.src(paths)
       .pipe(concat('vendor.js'))
       .pipe(__PROD__ ? uglify() : gutil.noop())
-      .pipe(gulp.dest(__DIST__ + '/js'))
+      .pipe(gulp.dest(__DIST__ + '/public/js'))
       .pipe(notify({ title: 'Vendor', message: 'Success', sound: 'Morse' }));
   });
 }
 
+var browserify = require('browserify');
+var watchify = require('watchify');
+var uglify = require('gulp-uglify');
+
+var shimify = require('browserify-shim');
+var reactify = require('reactify');
+var es6ify = require('6to5ify');
+
+/**
+ * Process scripts
+ * from src/scripts to dist/public/js
+ *
+ * @function scripts
+ */
 function scripts () {
-  var browserify = require('browserify');
-  var watchify = require('watchify');
-  var uglify = require('gulp-uglify');
-
-  var shimify = require('browserify-shim');
-  var reactify = require('reactify');
-  var es6ify = require('6to5ify');
-
   var bundler = browserify({
     cache: {},
     packageCache: {},
@@ -73,7 +86,7 @@ function scripts () {
       .pipe(source('bundle.js'))
       .pipe(__PROD__ ? buffer() : gutil.noop())
       .pipe(__PROD__ ? uglify() : gutil.noop())
-      .pipe(gulp.dest(__DIST__ + '/js'))
+      .pipe(gulp.dest(__DIST__ + '/public/js'))
       .pipe(notify({ title: 'Scripts', message: 'Success', sound: 'Morse' }));
   }
 
@@ -89,6 +102,12 @@ var less = require('gulp-less');
 var minify = require('gulp-minify-css');
 var sourcemaps = require('gulp-sourcemaps');
 
+/**
+ * Process less
+ * from src/styles to dist/public/css
+ *
+ * @function styles
+ */
 function styles () {
   function bundle () {
     return gulp.src(__SRC__ + '/styles/main.less')
@@ -99,7 +118,7 @@ function styles () {
       }))
       .pipe(__PROD__ ? minify() : gutil.noop())
       .pipe(__DEBUG__ ? sourcemaps.write() : gutil.noop())
-      .pipe(gulp.dest(__DIST__ + '/css'))
+      .pipe(gulp.dest(__DIST__ + '/public/css'))
       .pipe(notify({ title: 'Styles', message: 'Success', sound: 'Morse' }));
   }
 
@@ -110,21 +129,16 @@ function styles () {
   return bundle();
 }
 
-function images () {
-  var imagemin = require('gulp-imagemin');
-
-  return gulp.src(__SRC__ + '/images/*')
-    .pipe(imagemin())
-    .pipe(gulp.dest(__DIST__ + '/img'))
-    .pipe(notify({ title: 'Images', message: 'Success', sound: 'Morse' }));
-}
-
+/**
+ * Process html
+ * from src/html to dist
+ *
+ * @function html
+ */
 function html () {
-  var minify = require('gulp-htmlmin');
-
   function bundle () {
     return gulp.src(__SRC__ + '/html/**/*.html')
-      .pipe(__PROD__ ? minify({
+      .pipe(__PROD__ ? htmlminify({
         collapseWhitespace: true,
         removeComments: true,
         minifyJS: true,
@@ -141,21 +155,67 @@ function html () {
   return bundle();
 }
 
+var imagemin = require('gulp-imagemin');
+
+/**
+ * Minify & copy images
+ * from src/assets/images to dist/public/img
+ *
+ * @function method images
+ */
+function images () {
+  return gulp.src(__SRC__ + '/assets/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest(__DIST__ + '/public/img'))
+    .pipe(notify({ title: 'Images', message: 'Success', sound: 'Morse' }));
+}
+
+var htmlminify = require('gulp-htmlmin');
+
+/**
+ * Copy assets except images
+ * from src/assets to dist/public
+ *
+ * @function assets
+ */
+function assets () {
+  gulp.src([
+      __SRC__ + '/assets/**/*.*',
+      '!' + __SRC__ + '/assets/images/**/*.*'
+    ], { base: __SRC__ + '/assets' })
+    .pipe(gulp.dest(__DIST__ + '/public'));
+}
+
+/**
+ * Clean dist folder
+ *
+ * @function clean
+ */
 function clean (c) {
   var del = require('del');
 
   del([__DIST__], c);
 }
 
-function serve () {
-  var connect = require('gulp-connect');
+var connect = require('gulp-connect');
 
+/**
+ * Serve static content from dist on port 8000
+ *
+ * @function serve
+ */
+function serve () {
   connect.server({ root: __DIST__, port: 8000 });
 }
 
-function deploy () {
-  var ghpages = require('gulp-gh-pages');
+var ghpages = require('gulp-gh-pages');
 
+/**
+ * Deploy dist to gh-pages
+ *
+ * @function deploy
+ */
+function deploy () {
   return gulp.src(__DIST__ + '/**/*')
     .pipe(ghpages())
     .pipe(notify({ title: 'Deploy', message: 'Success', sound: 'Morse' }));
@@ -166,13 +226,14 @@ gulp.task('scripts', scripts);
 gulp.task('styles', styles);
 gulp.task('images', images);
 gulp.task('html', html);
+gulp.task('assets', assets);
 gulp.task('clean', clean);
 gulp.task('serve', serve);
 gulp.task('deploy', deploy);
 
 gulp.task('build', function () {
   __WATCH__ = false;
-  [clean, vendor, scripts, styles, html, images].forEach(function (fn) { fn(); });
+  [clean, vendor, scripts, styles, html, images, assets].forEach(function (fn) { fn(); });
 });
 
 gulp.task('watch', function () {
